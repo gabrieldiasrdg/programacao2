463d19d03e98f99a1452119a5571c14086f01ea2
gabrieldiasrdg <astronic.biel@gmail.com>
2025-09-22 08:49:40 -0300

    Add EX3

--- src/lista3/EX3.java
+++ src/lista3/EX3.java
@@ -1,5 +1,47 @@
+package lista3;
+
+import java.util.Scanner;
+
 public class EX3 {
+    public static int subStringOcorrencia (String frase, String subString) {
+        int cont = 0;
+        boolean ehSubPalavra = true;
+
+        for (int i = 0; i < frase.length() - subString.length(); i++) {
+            if (frase.charAt(i) == subString.charAt(0)) {
+                for (int j = 0; j < subString.length(); j++) {
+                    if(frase.charAt(i+j) != subString.charAt(j)) {
+                        ehSubPalavra = false;
+                        break;
+                    } else {
+                        ehSubPalavra = true;
+                    }
+                }
+                if (ehSubPalavra) {
+                    cont++;
+                    ehSubPalavra = false;
+                }
+            }
+        }
+
+        return cont;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        String frase="", subString="";
+
+        do {
+            System.out.println("Escreva a frase: ");
+            frase = sc.nextLine().toUpperCase();
+            System.out.println("Escreva a subString: ");
+            subString = sc.nextLine().toUpperCase();
+            if (frase.length() < subString.length()) {
+                System.out.println("A frase precisa ser maior que a subString!");
+            }
+        } while (frase.length() < subString.length());
+
+        System.out.printf("A quantidade de vezes que a subString '%s' aparece na frase é: %d vezes!", subString, subStringOcorrencia(frase, subString));
+
     }
 }
--- src/lista3/EX7.java
+++ src/lista3/EX7.java
@@ -1,3 +1,5 @@
+package lista3;
+
 public class EX7 {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
