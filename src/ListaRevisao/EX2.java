bd0e00347cfcf1ba1e92f22fab3d89404c0e44a8
gabrieldiasrdg <astronic.biel@gmail.com>
2025-10-02 19:32:03 -0300

    EXERCÍCIOS

--- src/ListaRevisao/EX2.java
+++ src/ListaRevisao/EX2.java
@@ -1,7 +1,25 @@
 package ListaRevisao;
 
+import java.util.Scanner;
+
 public class EX2 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        String[] comandos = {"ESCREVA", "LEIA", "PUXE", "PULE", "CALCULE", "TRANSFIRA", "SENAO", "ESCOLHA", "ENQUANTO", "FACA"};
+        String[] expressoesLogicas = {"AND", "OR", "NOT"};
+        String codigo = "";
+
+        System.out.println("Insira o código: ");
+        codigo = sc.nextLine().toUpperCase();
+        String[] vetorCodigo = codigo.split("\\s+");
+
+        for (int i = 0; i < vetorCodigo.length; i++) {
+            if (vetorCodigo[i].equals(codigo)) {
+
+            }
+        }
+
+
+
     }
 }
