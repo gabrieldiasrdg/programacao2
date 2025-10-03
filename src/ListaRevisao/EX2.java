93a1482234e4c27a879b8e62dace307ef7d4fd93
gabrieldiasrdg <astronic.biel@gmail.com>
2025-10-03 11:02:45 -0300

    Add EX3

--- src/ListaRevisao/EX2.java
+++ src/ListaRevisao/EX2.java
@@ -6,16 +6,19 @@ public class EX2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] comandos = {"ESCREVA", "LEIA", "PUXE", "PULE", "CALCULE", "TRANSFIRA", "SENAO", "ESCOLHA", "ENQUANTO", "FACA"};
-        String[] expressoesLogicas = {"AND", "OR", "NOT"};
+        String[] expressoesLogicas = {"AND", "OR", "NOT", "(", ")"};
         String codigo = "";
+        int comandosErrados = 0, comandosCertos = 0;
 
         System.out.println("Insira o código: ");
         codigo = sc.nextLine().toUpperCase();
         String[] vetorCodigo = codigo.split("\\s+");
 
         for (int i = 0; i < vetorCodigo.length; i++) {
-            if (vetorCodigo[i].equals(codigo)) {
+            for (int j = 0; j < comandos.length; j++) {
+                if(!vetorCodigo[i].equals(comandos[j]) && !vetorCodigo[i].equals(expressoesLogicas[j])) {
 
+                }
             }
         }
 
