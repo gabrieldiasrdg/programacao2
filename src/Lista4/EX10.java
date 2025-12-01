f65a9a83c15f3f9f5e2039d1ae6a58e7cd36589c
gabrieldiasrdg <astronic.biel@gmail.com>
2025-12-01 16:18:49 -0300

    Lista completa

--- src/Lista4/EX10.java
+++ src/Lista4/EX10.java
@@ -2,6 +2,17 @@ package Lista4;
 
 public class EX10 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int discos = 5;
+        hanoi(discos, 'A', 'B', 'C');
+    }
+
+    private static void hanoi(int discos, char origem, char auxiliar, char destino) {
+        if (discos > 0) {
+            hanoi(discos - 1, origem, destino, auxiliar); //origem to auxiliar
+            System.out.printf("Mover de %s para %s%n", origem, destino);
+            hanoi(discos - 1, auxiliar, origem, destino); //auxiliar to auxiliar
+        }
     }
 }
+
+//EU RECORRI A UM VÍDEO NO YOUTUBE, POIS NÃO ESTAVA ENTENDENDO MUITO BEM...
--- src/Lista4/EX8.java
+++ src/Lista4/EX8.java
@@ -6,22 +6,24 @@ public class EX8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Insira uma palavra: ");
-        String p = sc.next();
+        String p = sc.next().toUpperCase();
         int i = 0;
 
-        if (ehPalidromoImpar(p, i)) {
+        if (ehPalidromo(p, i)) {
             System.out.printf("%s é um palíndromo", p);
-        } else if (!ehPalidromoImpar(p, i)) {
+        } else if (!ehPalidromo(p, i)) {
             System.out.printf("%s NÃO é um palíndromo", p);
         }
 
     }
-    private static boolean ehPalidromoImpar(String p, int i) {
+    private static boolean ehPalidromo(String p, int i) {
         if ((p.length()+1)/2 == i) {
+            return true;
+        } else if (i == p.length()-1) {
             return true;
         } else if (p.charAt(i) != p.charAt(p.length()-(i+1))) {
             return false;
         }
-        return ehPalidromoImpar(p, i++);
+        return ehPalidromo(p, i+1);
     }
 }
